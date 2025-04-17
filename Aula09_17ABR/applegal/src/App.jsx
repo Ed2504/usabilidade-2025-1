944df5873ffedb8502f50ba2ac44d76af3420d86
mmamorim <marc.m.amorim@gmail.com>
2025-04-17 11:47:21 -0300

    v1

---
+++ Aula09_17ABR/applegal/src/App.jsx
@@ -0,0 +1,17 @@
+import Caixa from "./components/Caixa"
+
+function App() {
+
+  return(
+    <div>
+      oi gente
+      <button>clique aqui</button>
+      <Caixa />
+      <Caixa />
+      <Caixa />
+      <Caixa />
+    </div>
+  )
+}
+
+export default App
---
+++ Aula09_17ABR/applegal/src/components/Caixa.jsx
@@ -0,0 +1,12 @@
+import "./caixa.css"
+
+export default function Caixa() {
+
+    return (
+        <>
+            <div className="border p-2 m-2 bg-gray-400 rounded"> 
+                eu sou uma Caixa
+            </div>
+        </>
+    )
+}
