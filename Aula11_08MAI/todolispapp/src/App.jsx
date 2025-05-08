6de00a5566f39e1159f4db3808ff21dd1cddfb63
mmamorim <marc.m.amorim@gmail.com>
2025-05-08 10:47:21 -0300

    v1

---
+++ Aula11_08MAI/todolispapp/src/App.jsx
@@ -0,0 +1,20 @@
+
+import TopBar from "./components/TopBar";
+import TaskForm from "./components/TaskForm";
+import TaskItem from "./components/TaskItem";
+
+function App() {
+
+  return (
+    <>
+      <TopBar />
+      <TaskForm />
+      <TaskItem />
+      <TaskItem />
+      <TaskItem />
+      <TaskItem />
+    </>
+  )
+}
+
+export default App
