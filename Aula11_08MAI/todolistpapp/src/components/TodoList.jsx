f94ac29ad5e92375e0a4bea310da9db1ef8cf6e6
mmamorim <marc.m.amorim@gmail.com>
2025-05-15 10:48:37 -0300

    v1

--- Aula11_08MAI/todolistpapp/src/components/TodoList.jsx
+++ Aula11_08MAI/todolistpapp/src/components/TodoList.jsx
@@ -2,7 +2,15 @@ import TaskForm from "./TaskForm";
 import TaskItem from "./TaskItem";
 
 export default function TodoList() {
-    const tasks = ["Uma tarefa", "Outra tarefa", "Outra tarefa", "Alguma coisa", "mais um" ]
+    const tasks = ["Uma tarefa", "Outra tarefa", "Alguma coisa", "mais um" ]
+
+    fetch("http://localhost:3000/tasks/").then((res) => {
+        console.log("res",res);
+        res.json().then((data) => {
+            console.log("data",data);  
+                      
+        })
+    })
 
     return (
         <>
