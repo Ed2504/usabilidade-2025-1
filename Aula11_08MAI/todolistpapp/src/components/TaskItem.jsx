f90d1e8f744a1bab22c56c85a6742b9b8c4bfa63
mmamorim <marc.m.amorim@gmail.com>
2025-05-15 11:57:02 -0300

    v1

--- Aula11_08MAI/todolistpapp/src/components/TaskItem.jsx
+++ Aula11_08MAI/todolistpapp/src/components/TaskItem.jsx
@@ -1,17 +1,24 @@
 import { Icon } from "@iconify/react";
 
-export default function TaskItem({ nome }) {
+export default function TaskItem({ nome, id, done }) {
 
     return (
         <>
             <div className="bg-gray-200 p-2 rounded m-2 flex items-center justify-between md:w-140">
                 <div>
-                    { nome }
+                    {nome}
                 </div>
                 <div className="flex items-center gap-2">
-                    <div className="border border-black rounded-2xl px-1.5 pt-1 bg-green-800 text-white">
-                        <Icon icon="mdi:check" />
-                    </div>
+                    {
+                        done ?
+                            <div className="border border-black rounded-2xl px-1.5 pt-1 bg-green-800 text-white">
+                                <Icon icon="mdi:check" />
+                            </div>
+                            :
+                            <div className="border border-black rounded-2xl px-1.5 pt-1 bg-gray-400 text-white">
+                                <Icon icon="mdi:check" />
+                            </div>
+                    }
                     <div className="border border-black rounded-2xl px-1.5 pt-1 bg-gray-400 text-white">
                         <Icon icon="mdi:pencil" />
                     </div>
--- Aula11_08MAI/todolistpapp/src/components/TodoList.jsx
+++ Aula11_08MAI/todolistpapp/src/components/TodoList.jsx
@@ -1,21 +1,33 @@
+import { useState, useEffect } from "react";
 import TaskForm from "./TaskForm";
 import TaskItem from "./TaskItem";
 
 export default function TodoList() {
-    const tasks = ["Uma tarefa", "Outra tarefa", "Alguma coisa", "mais um" ]
+    const [tasks, setTasks] = useState([])
 
-    fetch("http://localhost:3000/tasks/").then((res) => {
-        console.log("res",res);
-        res.json().then((data) => {
-            console.log("data",data);  
-                      
-        })
-    })
+    async function load() {
+        let res = await fetch("http://localhost:3000/tasks/")
+        let data = await res.json()
+        console.log("data", data);
+        let vetData = []
+        for(let key in data) {
+            console.log("item",data[key]);            
+            vetData.push(data[key])
+        }
+        console.log("vetData", vetData);
+        setTasks(vetData)
+    }
+
+    useEffect(() => {
+        load()
+    },[])
 
     return (
         <>
             <TaskForm />
-            { tasks.map((tarefa) => <TaskItem nome={tarefa} key={tarefa} /> ) }
+            {
+                tasks.map((tarefa) => <TaskItem nome={tarefa.nome} key={tarefa.id} done={tarefa.done} />)
+            }
         </>
     )
 }
